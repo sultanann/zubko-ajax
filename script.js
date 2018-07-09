0b491b9a8fd2f0e4ad76d36959bf6e08709d8e14
sultanann <39970875+sultanann@users.noreply.github.com>
2018-07-09 16:29:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
 $('.ok').on('click', function(e){
     e.preventDefault();
-   var artName = $('.artName').val(), // берем val <select name="" class="country"><option value="0">-</option>
-       text = $('.text').val(); // берем val <select name="" class="country"><option value="0">-</option>
-    $.ajax({                 //ajax запрос формируется обьектами
-        url:'post.php',      //к какому файлу мы передаем запрос, путь к php
-        type:'POST',         //запрос get-и получить и послать, post-только послать
-        data:{artName: artName, text: text}, //данные, что мы отсылаем(обьект). country-ключ что я отсылаю, он может быть с любым названием
-        dataType:'html',  //какие данные мы отсылаем
-        beforeSend: function(){  // что мы делаем пока ждем ответ от сервера
-            $('.windows8').fadeIn();//запускаем preloader
+   var artName = $('.artName').val(), 
+       text = $('.text').val(); 
+    $.ajax({                 
+        url:'post.php',      
+        type:'POST',         
+        data:{artName: artName, text: text}, 
+        dataType:'html',  
+        beforeSend: function(){  
+            $('.windows8').fadeIn();
         },
-        success:function(data){ //приходит в кодировке за счет json_encode  {"1":"\u0427\u043e\u043f","2":"\u0414\u043d\u0435\u043f\u0440"}
+        success:function(data){ 
             $('.windows8').fadeOut();
             $('.articles').html(data);
 
